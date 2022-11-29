46e22735429870c99364f16072955c2e0fe7643b
teacher <itstep.codeclub@gmail.com>
2022-11-29 18:52:11 +0200

    class MenuManager - done

--- MenuManager.cs
+++ MenuManager.cs
@@ -4,6 +4,28 @@ namespace Calculator
 {
     class MenuManager
     {
-        // ...
+        public void Display() {
+            Console.WriteLine("\n\t ==========================");
+            Console.WriteLine("\n\t  Арифметичний калькулятор ");
+            Console.WriteLine("\n\t ==========================");
+            Console.WriteLine("\n\t      1 - Додавання        ");
+            Console.WriteLine("\n\t      2 - Віднімання       ");
+            Console.WriteLine("\n\t      3 - Множення         ");
+            Console.WriteLine("\n\t      4 - Ділення          ");
+            Console.WriteLine("\n\t      5 - Вихід            ");
+            Console.WriteLine("\n\t ==========================");
+        }
+
+        public int GetChoice() {
+            Console.Write("\n> Вкажіть номер потрібної Вам операції: ");
+            int choice = int.Parse(Console.ReadLine());
+            return choice;
+        }
+
+        public bool AllowContinue() {
+            Console.Write("\n> Продовжити (y/n)? - ");
+            string allow = Console.ReadLine(); 
+            return (allow == "y");
+        }
     }
 }
